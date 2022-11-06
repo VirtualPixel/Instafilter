405a427fb575d067a319d2360fbc269ca3b842a1
Justin Wells <wellsmjustin@gmail.com>
2022-11-06 18:28:37 -0500

    updated code

--- Instafilter/ImagePicker.swift
+++ Instafilter/ImagePicker.swift
@@ -9,11 +9,37 @@ import PhotosUI
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            // dismiss the picker
+            picker.dismiss(animated: true)
+            
+            // exit if no selection was made
+            guard let provider = results.first?.itemProvider else { return }
+            
+            // if this is an image we can use, use it
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+    }
+    
+    @Binding var image: UIImage?
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
         
         let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
         return picker
     }
     
@@ -21,5 +47,7 @@ struct ImagePicker: UIViewControllerRepresentable {
         
     }
     
-    typealias UIViewControllerType = PHPickerViewController
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 }
